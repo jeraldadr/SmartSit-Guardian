27d22aac0dadf053f20fdf639083bbf548184339
Jianming Zheng <jianmiz1@uci.edu>
2024-11-20 15:06:35 -0800

    add sensor files

---
+++ Documents/PlatformIO/Projects/final_project/src/Accelerometer.cpp
@@ -0,0 +1,114 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <string>
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM6DSO.h>
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+// Pin 
+const int LED = 26;
+
+LSM6DSO myIMU; //Default constructor is I2C, addr 0x6B
+
+// BLE variables
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+BLEService *pService = NULL;
+
+// Time process
+unsigned long startTime; 
+unsigned long runTime;
+const unsigned long calibrationTime = 10000; // 10s calibration
+
+
+float min_X;
+float max_X;
+float x_val; 
+const int THRESHOLD_VALUE = map(1.5, -2, 2, 0, 255);   
+uint32_t stepCount = 0;
+
+void setup() {
+  Serial.begin(9600);
+ 
+  // LSM6DSO Sensor Set-Up
+  delay(500); 
+
+  Wire.begin();
+  delay(10);
+  if( myIMU.begin() )
+    Serial.println("Ready.");
+  else { 
+    Serial.println("Could not connect to IMU.");
+    Serial.println("Freezing");
+  }
+
+  if( myIMU.initialize(BASIC_SETTINGS) )
+    Serial.println("Loaded Settings.");
+
+
+  // BLE Device Set-Up
+  Serial.println("Starting BLE work!");
+  BLEDevice::init("CS147");
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID,
+                                        BLECharacteristic::PROPERTY_READ |
+                                        BLECharacteristic::PROPERTY_WRITE
+  );
+
+  pCharacteristic->setValue("Server Starts"); //What gets transfered
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x0);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+
+  // Calibration Phrase Needed
+  startTime = millis(); 
+  min_X = 0.0; 
+  max_X = 0.0; 
+  pinMode(LED, OUTPUT); 
+}
+
+void loop() {
+  runTime = millis();
+
+  // calibration phrase
+  if ((runTime - startTime) < calibrationTime) {
+      digitalWrite(LED, HIGH);
+      float x = myIMU.readFloatAccelX();
+      min_X = min(min_X, x);
+      max_X = max(max_X, x);
+  }
+  else {
+    digitalWrite(LED, LOW);
+    x_val = myIMU.readFloatAccelX();
+    x_val = (x_val - min_X) * (255 - 0) / (max_X - min_X); 
+
+    bool stepDetected = false; 
+    if (x_val > THRESHOLD_VALUE && !stepDetected) {
+        stepCount++;
+        stepDetected = true;
+        Serial.println("Step detected! Total steps: " + String(stepCount));
+        
+        String stepCountString = "Steps: " + String(stepCount);
+        
+    pCharacteristic->setValue(stepCountString.c_str());
+    pCharacteristic->notify();
+
+    } else if (x_val < THRESHOLD_VALUE) {
+        stepDetected = false;  // Reset step detection
+    }
+    delay(20);  
+  }
+  
+  delay(1000);
+}
---
+++ Documents/PlatformIO/Projects/final_project/src/TouchSensor.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_CAP1188.h>
+#include <TFT_eSPI.h>
+
+
+// Part 2 (Hardware SPI)
+
+// Define CAP1188 object for the touch sensor
+Adafruit_CAP1188 cap = Adafruit_CAP1188();
+
+// Define TFT_eSPI object for the display purpose
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("CAP1188 test!");
+  tft.begin();
+  tft.setRotation(1);
+
+  
+  if (!cap.begin(0x29)) {
+    Serial.println("CAP1188 not found");
+    while (1);
+  }
+  Serial.println("CAP1188 found!");
+}
+
+void loop() {
+  u_int8_t count = 0;
+  uint8_t touched = cap.touched();
+
+  if (touched == 0) {
+    count = 0;
+  }
+  
+  for (uint8_t i=0; i<8; i++) {
+    if (touched & (1 << i)) {
+      count++;
+    }
+  }
+
+  Serial.println();
+  delay(50);
+  tft.setTextSize(10);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setCursor(0,15);
+  tft.println(count);
+}
