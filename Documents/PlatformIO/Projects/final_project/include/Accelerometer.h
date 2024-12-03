864bf7a5a697747e56d5e6ef11ce697e0f00f1ff
Jerald Adriano <jaadrian@uci.edu>
2024-12-03 11:04:43 -0800

    Fixed header files and moved them to the include folder

--- Documents/PlatformIO/Projects/final_project/include/Accelerometer.h
+++ Documents/PlatformIO/Projects/final_project/include/Accelerometer.h
@@ -1,21 +1,22 @@
 #ifndef ACCELEROMETER_H
 #define ACCELEROMETER_H
 
-#include <Adafruit_LSM6DSO.h>
+#include <SparkFunLSM6DSO.h>
 
+const unsigned long calibrationTime = 10000; // 10s calibration
+const int THRESHOLD_VALUE = map(1.5, -2, 2, 0, 255);   // threshold
 
-// Declarations
-void initAccel(LSM6DSO &myIMU); 
-/*Initializes the LSM6DSO in order to use the accelerometer*/
+// Initialize the accelerometer 
+void initAccel(LSM6DSO &myIMU);
 
-void calibrateAccel(float &min_X, float &max_X);
-/*10s calibration in order to determine the minimum x and 
-maximum x values to be used for the threshold*/
+// Calibrate Operation
+// calibrate 10s for accelerometer value mapping 
+void calibrateAccel(LSM6DSO &myIMU, float &min_X, float &max_X);
 
-float readAccel();
-/*Reads accelerometer value from LSM6DSO and maps it using a custom mapping equation*/
+// Read the data from the accelerometer and map it
+float readAccel(LSM6DSO &myIMU, float &min_X, float &max_X);
 
+// Determine whether the user sitting on the object
+bool onPosition(float x_val);
 
-
-#endif
-
+#endif // ACCELEROMETER_H
--- Documents/PlatformIO/Projects/final_project/include/TouchSensor.h
+++ Documents/PlatformIO/Projects/final_project/include/TouchSensor.h
@@ -1,13 +1,21 @@
 #ifndef TOUCHSENSOR_H
 #define TOUCHSENSOR_H
+
 #include <Adafruit_CAP1188.h>
 #include <TFT_eSPI.h>
+#include <SPI.h>
 
-void touchinit(Adafruit_CAP1188 &cap, TFT_eSPI &tft);
-/*Initilizes Adafruit_CAP1188 touch sensor*/
+// Define necessary pins for touch sensor
+#define CAP1188_CS  10
+#define CAP1188_MOSI  11
+#define CAP1188_MISO  12
+#define CAP1188_CLK  13
+#define CAP1188_RESET  9
 
-void readTouch(Adafruit_CAP1188 &cap, TFT_eSPI &tft);
+// Initialize the touch sensor
+void initTouchSensor(Adafruit_CAP1188 &cap, TFT_eSPI &tft);
 
+// Detect whether the user touch the sensor
+bool readTouch(Adafruit_CAP1188 &cap, TFT_eSPI &tft);
 
-
-#endif
+#endif // TOUCHSENSOR_H
